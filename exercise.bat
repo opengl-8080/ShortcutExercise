9a9186e99159b58c539effca20558420debfbf6b
opengl8080 <tomcat.port.8080+github@gmail.com>
2014-09-27 16:05:14 +0900

    move preset dir

--- exercise.bat
+++ exercise.bat
@@ -3,9 +3,9 @@
 set THIS_DIR=%~dp0
 set THIS_DIR=%THIS_DIR:~,-1%
 set SRC_DIR=%THIS_DIR%\src
+set SCRIPT_FILE="%SRC_DIR%\Main.groovy"
+set PRESET_DIR="%THIS_DIR%\preset"
 set CLASSPATH=%SRC_DIR%
 
-call groovy -Dpreset.dir="%THIS_DIR%" "%SRC_DIR%\Main.groovy"
-
-
+call groovy -Dpreset.dir=%PRESET_DIR% %SCRIPT_FILE%
 
